04efbab8cee80e7db26e7de9788af62334f3dfb6
Digitaneo <digitaneo@gmail.com>
2026-08-03 03:14:07 +0100

    test api 4

--- auth.js
+++ auth.js
@@ -1,15 +1,20 @@
-import crypto from "node:crypto";
-
 export function generateNonce() {
-    return crypto.randomBytes(16).toString("hex");
+
+    const bytes = crypto.getRandomValues(
+        new Uint8Array(16)
+    );
+
+    return [...bytes]
+        .map(x => x.toString(16).padStart(2, "0"))
+        .join("");
+
 }
 
 export function buildInstallUrl(shop, env, nonce) {
 
-    const scopes = env.SCOPES;
-
-    const redirectUri =
+    const redirect =
         `${env.APP_URL}/callback`;
 
-    return `https://${shop}/admin/oauth/authorize?client_id=${env.SHOPIFY_API_KEY}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${nonce}`;
+    return `https://${shop}/admin/oauth/authorize?client_id=${env.SHOPIFY_API_KEY}&scope=${encodeURIComponent(env.SCOPES)}&redirect_uri=${encodeURIComponent(redirect)}&state=${nonce}`;
+
 }
